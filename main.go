1152c1b9f59d0dab9931c3c762ea777db7abdf4f
Naoto Kaneko <naoty.k@gmail.com>
2017-10-10 21:40:40 +0900

    Accept input from STDIN

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	fmt.Println("shbot> ")
+	fmt.Print("shbot> ")
+
+	reader := bufio.NewReader(os.Stdin)
+	str, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	fmt.Println(str)
+
 	os.Exit(0)
 }
