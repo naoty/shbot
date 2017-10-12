55401ec15ad1d40048fdb7bcb649e3ff99034cf1
Naoto Kaneko <naoty.k@gmail.com>
2017-10-12 11:59:35 +0900

    Add --help and -h option to show help message

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 var version = "0.1.0"
@@ -13,9 +14,26 @@ func main() {
 		case "-v", "--version":
 			fmt.Println(version)
 			os.Exit(0)
+		case "-h", "--help":
+			fmt.Println(help())
+			os.Exit(0)
 		}
 	}
 
 	bot := &Bot{Input: os.Stdin, Output: os.Stdout, ErrorOutput: os.Stderr}
 	bot.Run()
+}
+
+func help() string {
+	lines := []string{}
+	lines = append(lines, "Usage:")
+	lines = append(lines, "  shbot")
+	lines = append(lines, "  shbot (--version | -v)")
+	lines = append(lines, "  shbot (--help | -h)")
+	lines = append(lines, "")
+	lines = append(lines, "Options:")
+	lines = append(lines, "  --version, -v\tShow version number")
+	lines = append(lines, "  --help, -h\tShow help message")
+
+	return strings.Join(lines, "\n")
 }
